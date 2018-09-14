f528a19616619c60e29ec21f9fc1b87dfca4a236
Boyd Ndonga <boydbugle@gmail.com>
2018-09-15 01:05:58 +0300

    create maskify for javascript

---
+++ javascript/maskify.js
@@ -0,0 +1,6 @@
+// usually when you buy something, you are asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+// maskify("4556364607935616") == "############5616"
+// maskify(     "64607935616") ==      "#######5616"
+// maskify(               "1") ==                "1"
+// maskify(                "") ==                 ""
