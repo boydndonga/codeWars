8f0c56537a86b377287814f65eceba739f282228
Boyd Ndonga <boydbugle@gmail.com>
2018-09-15 03:14:23 +0300

    create pytho dashatize solution

--- python/dashitize.py
+++ python/dashitize.py
@@ -2,3 +2,16 @@
 # after each odd integer, but do not begin or end the string with a dash mark.
 # dashatize(274) -> '2-7-4'
 # dashatize(6815) -> '68-1-5'
+
+def dashatize(num):
+   if isinstance(num,int):
+       a=list(str(abs(num)))
+       d=''.join(['-'+x+'-' if int(x)%2!=0 else x for x in a])
+       if d[len(d)-1]=='-':
+           d=d[:-1]
+       if d[0]=='-':
+           d=d[1:]
+       d=d.replace('--','-')
+       return d
+   else:
+       return 'None'
